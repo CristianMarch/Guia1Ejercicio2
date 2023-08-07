9241ee53719b8a60ec016a8be0986ea410578ba9
diakza <diegodearmas01@gmail.com>
2023-08-07 12:36:54 -0300

    Update Guia1Ejercicio2.java
    ingrese un comentario

--- src/main/java/com/mycompany/guia1ejercicio2/Guia1Ejercicio2.java
+++ src/main/java/com/mycompany/guia1ejercicio2/Guia1Ejercicio2.java
@@ -13,5 +13,6 @@ public class Guia1Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hola, acá Lala");
+        System.out.println(" hola aca diego");
     }
 }
