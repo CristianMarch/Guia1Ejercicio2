e019abacb4ff2ee2beff1ceb83f8fc57aa721c58
CristianMarch <kicho2002@hotmail.com>
2023-08-07 12:22:10 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/guia1ejercicio2/Guia1Ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia1ejercicio2;
+
+/**
+ *
+ * @author kicho
+ */
+public class Guia1Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
